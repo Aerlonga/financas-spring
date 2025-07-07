be19b98669f76c7330c4f22834d462a6bbefa577
Aerlonga <aerlon.as@gmail.com>
2025-07-06 21:06:44 -0300

    add

--- src/main/java/dev/financas/FinancasSpring/Controller.java
+++ src/main/java/dev/financas/FinancasSpring/Controller.java
@@ -1,4 +1,15 @@
 package dev.financas.FinancasSpring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/primeirarota")
+    public String primeiraRota(){
+        return "Funcionando!";
+    }
 }
