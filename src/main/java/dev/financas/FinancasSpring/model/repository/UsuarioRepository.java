b82cd127ef0b1498e6b1d3537b926acfe48d62ab
Aerlonga <aerlon.as@gmail.com>
2025-10-05 22:01:09 -0300

    adiciona validação de CPF único e relacionamentos no modelo de usuário, incluindo DTOs e repositórios correspondentes

--- src/main/java/dev/financas/FinancasSpring/model/repository/UsuarioRepository.java
+++ src/main/java/dev/financas/FinancasSpring/model/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package dev.financas.FinancasSpring.model.repository;
 
 import dev.financas.FinancasSpring.model.entities.Usuario;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +10,8 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    // Busca o usuário já carregando os relacionamentos
+    @EntityGraph(attributePaths = { "detalhes", "financeiro", "preferencias" })
+    Optional<Usuario> findWithAllRelationsByEmail(String email);
 }
